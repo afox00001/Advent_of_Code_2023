97e83ab6469a54db8595043dbaa0e219a9e2e7f4
Ashton Fox <ashscottv@gmail.com>
2025-07-08 13:35:29 -0500

    added if __name__ == "__main__": to Day 7 part1.py

--- Day 7/part1.py
+++ Day 7/part1.py
@@ -1,15 +1,16 @@
 from Hand import *
 
-hands = []
-with open("input.txt") as file:
-    for line in file:
-        parts = line.strip().split()
-        cards = list(parts[0])  # Converts "2345K" into ['2','3','4','5','K']
-        bet = int(parts[1])
-        hands.append(Hand(cards, bet))
-hands.sort()
+if __name__ == "__main__":
+    hands = []
+    with open("input.txt") as file:
+        for line in file:
+            parts = line.strip().split()
+            cards = list(parts[0])  # Converts "2345K" into ['2','3','4','5','K']
+            bet = int(parts[1])
+            hands.append(Hand(cards, bet))
+    hands.sort()
 
-total = 0
-for i, hand in enumerate(hands):
-    total += hand.bet * (i + 1)
-print(f"Sum: {total}")
+    total = 0
+    for i, hand in enumerate(hands):
+        total += hand.bet * (i + 1)
+    print(f"Sum: {total}")
