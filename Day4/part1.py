164633e069ef431f5759c0ea4accc4e64ec5360c
Ashton Fox <ashscottv@gmail.com>
2025-07-08 13:54:48 -0500

    Made Day 4 Part 1 more readable.

--- Day4/part1.py
+++ Day4/part1.py
@@ -1,20 +1,16 @@
 if __name__ == "__main__":
-    with open("input.txt", "r") as input:
-        input_text_lines = input.readlines()
-        total_points = 0
-        for ticket in input_text_lines:
-            cards_points = 0
-            winning_numbers = ticket.replace("\n", "").split(":")[1].split("|")[0].split(" ")
-            winning_numbers = list(filter(("").__ne__, winning_numbers))
+    total_points = 0
 
-            card_numbers = ticket.replace("\n", "").split(":")[1].split("|")[1].split(" ")
-            card_numbers = list(filter(("").__ne__, card_numbers))
+    with open("input.txt", "r") as file:
+        for ticket in file:
+            ticket = ticket.strip()
+            numbers_section = ticket.split(":")[1]
+            winning_numbers = numbers_section.split("|")[0].split()
+            card_numbers = numbers_section.split("|")[1].split()
 
-            for card_number in card_numbers:
-                if card_number in winning_numbers:
-                    if cards_points == 0:
-                        cards_points = 1
-                    else:
-                        cards_points *= 2
-            total_points += cards_points
-        print(total_points)
+            match_count = sum(1 for num in card_numbers if num in winning_numbers)
+
+            if match_count > 0:
+                total_points += 2 ** (match_count - 1)
+
+    print(total_points)
