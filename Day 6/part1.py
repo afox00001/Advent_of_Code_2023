b68211cc99bed9b8b85c3e70b7607e169961207d
Ashton Fox <ashscottv@gmail.com>
2025-07-08 14:08:59 -0500

    Made Day 6 Part 1 to be more readable.

--- Day 6/part1.py
+++ Day 6/part1.py
@@ -1,34 +1,17 @@
 with open("input.txt", "r") as input_file:
-    input_file_lines = input_file.readlines()
+    lines = [line.split(":")[1].strip().split() for line in input_file]
 
-    lines = []
-    for line in input_file_lines:
-        lines.append(list(filter(("").__ne__, line.split(":")[1].replace("\n", "").split(" "))))
-    times_str = lines[0]
-    distances_str = lines[1]
+times = list(map(int, lines[0]))
+distances = list(map(int, lines[1]))
 
-    times = []
-    distances = []
-    for time in times_str:
-        times.append(int(time))
-    for distance in distances_str:
-        distances.append(int(distance))
+product = 1
 
-    greatest_distances = []
-    list_of_ways_to_win = []
-    i = 0
-    for time in times:
-        greatest_distance = distances[i]
-        number_of_ways_to_win = 0
-        i += 1
-        for speed in range(time + 1):
-            distance = (time - speed) * speed
-            if distance > greatest_distance:
-                number_of_ways_to_win += 1
-        list_of_ways_to_win.append(number_of_ways_to_win)
-        greatest_distances.append(greatest_distance)
+for time, record_distance in zip(times, distances):
+    ways_to_win = 0
+    for speed in range(time + 1):
+        distance = speed * (time - speed)
+        if distance > record_distance:
+            ways_to_win += 1
+    product *= ways_to_win
 
-    product = 1
-    for i in list_of_ways_to_win:
-        product *= i
-    print(product)
+print(product)
